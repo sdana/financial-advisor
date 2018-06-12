629076d2723b65faea39f336dedee997cd382e91
sdana <seth.dana@gmail.com>
2018-06-12 11:35:39 -0500

    Added new key object containing holdings, added new method to modify holdings object

--- main.js
+++ main.js
@@ -29,6 +29,11 @@ const finAdv = Object.create({},{
         writeable: true,
         value: []
     },
+    holdings: {
+        enumerable: true,
+        writable: true,
+        value: {}
+    },
     netWorth: {
         enumerable: true,
         writeable:  true,
@@ -51,18 +56,62 @@ const finAdv = Object.create({},{
             this.portfolio.push(newTrans)
     }
     },
+    modifyHoldings: {
+        //create new transaction, paramaters: (String, Int, Int, String)
+        value: function (ticker, numShares, value, buyOrSell){
+            let transType
+            switch (buyOrSell){
+                case ("buy"):
+                    //check to see if ticker exists, if so modify number of shares
+                    if (this.holdings[ticker]){    
+                    this.holdings[ticker] = {symbol: ticker, shares: +numShares+this.holdings[ticker].shares, price: value,}
+                    break
+                    }
+                    //if the ticker does not exist, create it
+                    else {
+                        this.holdings[ticker] = {symbol: ticker, shares: numShares, price: value,}
+                    break
+                    }    
+
+                case ("sell"):
+                    //ensure ticker to modify exists
+                    if (!this.holdings[ticker]){
+                        break
+                    }
+                    else if (this.holdings[ticker].shares-numShares < 0){
+                        console.log("Cannot sell more than you own")
+                        break
+                    }
+                    //if ticker exists, and is greater than 0 then modify the number of shares
+                    else if (this.holdings[ticker].shares && this.holdings[ticker].shares > 0){
+                        this.holdings[ticker] = {symbol: ticker, shares: this.holdings[ticker].shares-numShares, price: value,}
+                        //if the modification causes the shares to reach 0, delete the ticker
+                        if (this.holdings[ticker].shares === 0){
+                            delete this.holdings[ticker]
+                        } 
+                        break
+                    }
+            }
+            let newTrans = {symbol: ticker, shares: numShares, price: value, own: transType}
+            this.portfolio.push(newTrans)
+    }
+    },
     worth: {
         value: function(){
+            //declare temporary variables to hold each stock's total worth and an array to hold that variable
             let tmpArr = []
             let numTimesValue = 0
+            //iterate through portfolio array
             for (let i=0; i<this.portfolio.length; i++){
+                //check to see if stock was bought or sold
                 if (this.portfolio[i].own === true){
-                    console.log("shares", this.portfolio[i].shares)
-                    console.log("value", this.portfolio[i].price)
+                    //if bought, multiply the number of shares by the price
                     numTimesValue = this.portfolio[i].shares * this.portfolio[i].price
+                    //push that value to temporary array
                     tmpArr.push(numTimesValue)
                 }
             }
+            //return the total value of the temporary array
             return `Current New Worth: ${tmpArr.reduce((current, next) =>{
                 return current+next
             })}`
